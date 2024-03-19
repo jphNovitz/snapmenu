251c0583cf99fa7746e58d45a34e162e13641e5f
Novitz Jean-Philippe <novitz@gmail.com>
2024-03-19 22:36:45 +0100

    Add CategoryControllerTest (category custom)
    Rename ProductControllerTest (was productControllerTest)

--- tests/Admin/CategoryControllerTest.php
+++ tests/Admin/CategoryControllerTest.php
@@ -1,12 +1,14 @@
 <?php
 
-namespace App\Tests\Admin;
+namespace Admin;
 
-use App\DataFixtures\tests\ProductFixtures;
+use App\DataFixtures\tests\CategoryFixtures;
 use App\DataFixtures\tests\UserFixtures;
+use App\Entity\Category;
+use App\Entity\CategoryCustom;
 use App\Entity\Product;
 use App\Entity\User;
-use App\Repository\ProductRepository;
+use App\Repository\CategoryRepository;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Hautelook\AliceBundle\PhpUnit\RefreshDatabaseTrait;
@@ -17,7 +19,7 @@ use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Faker;
 
-class productControllerTest extends WebTestCase
+class CategoryControllerTest extends WebTestCase
 {
     use RefreshDatabaseTrait;
 
@@ -27,8 +29,8 @@ class productControllerTest extends WebTestCase
     protected AbstractDatabaseTool $databaseTool;
     private KernelBrowser $client;
     private UserRepository $userRepository;
-    private ProductRepository $productRepository;
-    private string $path = '/admin/product/';
+    private CategoryRepository $categoryRepository;
+    private string $path = '/admin/category/';
 
     private EntityManagerInterface $entityManager;
     private $faker;
@@ -37,13 +39,13 @@ class productControllerTest extends WebTestCase
     {
         $this->client = static::createClient();
         $this->userRepository = static::getContainer()->get('doctrine')->getRepository(User::class);
-        $this->productRepository = static::getContainer()->get('doctrine')->getRepository(Product::class);
+        $this->categoryRepository = static::getContainer()->get('doctrine')->getRepository(Category::class);
         $this->entityManager = static::getContainer()->get('doctrine')->getManager();
         $this->databaseTool = static::getContainer()->get(DatabaseToolCollection::class)->get();
         $this->faker = Faker\Factory::create();;
     }
 
-    public function test_admin_product_is_not_accessible_anonymously(): void
+    public function test_admin_category_is_not_accessible_anonymously(): void
     {
         $this->client->request('GET', $this->path);
 
@@ -51,135 +53,108 @@ class productControllerTest extends WebTestCase
         $this->assertResponseRedirects('/login');
     }
 
-    public function test_admin_product_index_is_accessible_if_logged(): void
+    public function test_admin_category_index_is_accessible_if_logged(): void
     {
         $this->databaseTool->loadFixtures([
             UserFixtures::class,
-            ProductFixtures::class]);
+            CategoryFixtures::class]);
 
         $user = $this->userRepository->findOneBy(['email' => 'admin@exempl.es']);
-        $product = $this->productRepository->findAll()[0];
+        $category = $this->categoryRepository->findOneBy(['name' => 'Lorem Custom']);
 
         $this->client->loginUser($user);
         $crawler = $this->client->request('GET', $this->path);
 
         $this->assertResponseStatusCodeSame(200);
         $this->assertResponseIsSuccessful();
-        $this->assertSelectorTextContains("h1", 'Liste des produits');
-        $this->assertStringContainsString($product->getName(), $crawler->text());
-        $this->assertStringContainsString($product->getDescription(), $crawler->text());
-        $this->assertStringContainsString($product->getIngredients(), $crawler->text());
-        $this->assertStringContainsString($product->getPriceHome(), $crawler->text());
+        $this->assertSelectorTextContains("h1", 'Liste des categories');
+        $this->assertStringContainsString($category->getName(), $crawler->text());
         $this->assertStringContainsString("Modifier", $crawler->text());
         $this->assertStringContainsString("Ajouter", $crawler->text());
     }
 
-    #[NoReturn] public function test_admin_can_add_a_product(): void
+    #[NoReturn] public function test_admin_can_add_a_category(): void
     {
         $this->databaseTool->loadFixtures([
             UserFixtures::class]);
 
-        $user = $this->userRepository->findOneBy(['email' => 'simple@exempl.es']);
+        $user = $this->userRepository->findOneBy(['email' => 'admin@exempl.es']);
         $this->client->disableReboot();
         $this->client->loginUser($user);
 
-        $crawler = $this->client->request('GET', sprintf('%snew', $this->path));
+        $this->client->request('GET', sprintf('%snew', $this->path));
 
 //        Page new is accessible
         $this->assertResponseStatusCodeSame(200);
         $this->assertResponseIsSuccessful();
-        $this->assertSelectorTextContains("h1", 'Ajouter un produit');
+        $this->assertSelectorTextContains("h1", 'Ajouter une categorie');
 
         $crawler = $this->client->submitForm('Ajouter', [
-            'product[name]' => $this->faker->name(),
-            'product[description]' => $this->faker->sentence(),
-            'product[ingredients]' => $this->faker->words(3, true),
-            'product[priceHome]' => '4,00',
-            'product[priceAway]' => '3,00',
-            'product[veggie]' => 1,
-            'product[halal]' => 1,
+            'category_custom[name]' => $this->faker->name(),
         ]);
 
         $this->assertResponseRedirects($this->path, 302);
-        $this->assertSame(1, $this->productRepository->count([]));
+        $this->assertSame(1, $this->categoryRepository->count([]));
     }
 
-    public function test_admin_can_update_a_product(): void
+    public function test_admin_can_update_a_category(): void
     {
         $this->databaseTool->loadFixtures([
             UserFixtures::class,
-            ProductFixtures::class]);
+            categoryFixtures::class]);
 
         $user = $this->userRepository->findOneBy(['email' => 'admin@exempl.es']);
-        $product = $this->productRepository->findAll()[0];
+        $product = $this->categoryRepository->findOneBy(['name' => 'Lorem Custom']);
 
         $this->client->disableReboot();
         $this->client->loginUser($user);
 
-        $crawler = $this->client->request('GET', sprintf('%s%s/edit', $this->path, $product->getId()));
+        $this->client->request('GET', sprintf('%s%s/edit', $this->path, $product->getId()));
 
 //        Page new is accessible
         $this->assertResponseStatusCodeSame(200);
         $this->assertResponseIsSuccessful();
-        $this->assertSelectorTextContains("h1", 'Modifier un produit');
+        $this->assertSelectorTextContains("h1", 'Modifier une categorie');
 
         $name = $this->faker->name();
-        $description = $this->faker->sentence();
-        $ingredients = $this->faker->words(3, true);
-        $price_home = '4,00';
-        $price_away = '3,00';
-        $veggie = $this->faker->boolean;
-        $halal = $this->faker->boolean;
 
 //        Submit the 'new product form'
         $this->client->submitForm('Modifier', [
-            'product[name]' => $name,
-            'product[description]' => $description,
-            'product[ingredients]' => $ingredients,
-            'product[priceHome]' => $price_home,
-            'product[priceAway]' => $price_away,
-            'product[veggie]' => $veggie,
-            'product[halal]' => $halal,
+            'category_custom[name]' => $name
         ]);
 
         $this->assertResponseStatusCodeSame(302);
         $this->assertResponseRedirects($this->path);
 
-        $product = $this->productRepository->findOneBy(['name' => $name]);
+        $category = $this->categoryRepository->findOneBy(['name' => $name]);
 
-        $this->assertSame($name, $product->getName());
-        $this->assertSame($description, $product->getDescription());
-        $this->assertSame($ingredients, $product->getIngredients());
-        $this->assertSame($price_home, $product->getPriceHome());
-        $this->assertSame($price_away, $product->getPriceAway());
-        $this->assertSame($veggie, $product->isVeggie());
-        $this->assertSame($halal, $product->isHalal());
-
+        $this->assertSame($name, $category->getName());
     }
 
-    public function test_admin_can_delete_a_product(): void
+    public function test_admin_can_delete_a_category(): void
     {
         $this->databaseTool->loadFixtures([
             UserFixtures::class,
-            ProductFixtures::class]);
+            CategoryFixtures::class]);
 
         $user = $this->userRepository->findOneBy(['email' => 'admin@exempl.es']);
 
 
-        $products = $this->productRepository->findAll();
-        $total = count($products);
+        $categories = $this->categoryRepository->findBy(['name' => 'Lorem Custom']);
+        $total = count($categories);
 
         $this->client->disableReboot();
         $this->client->loginUser($user);
-        $crawler = $this->client->request('GET', sprintf('%s%s/edit', $this->path, $products[0]->getId()));
+        $this->client->request('GET', sprintf('%s%s/edit', $this->path, $categories[0]->getId()));
 
 //        Page new is accessible
         $this->assertResponseStatusCodeSame(200);
         $this->assertResponseIsSuccessful();
-        $this->assertSelectorTextContains("h1", 'Modifier un produit');
+        $this->assertSelectorTextContains("h1", 'Modifier une categorie');
 
         $this->client->submitForm('Supprimer');
         $this->assertResponseRedirects($this->path, 302);
+        $this->assertCount($total - 1, $this->categoryRepository->findBy(['name' => 'Lorem Custom']));
 
     }
 
---
+++ tests/Admin/ProductControllerTest.php
@@ -0,0 +1,191 @@
+<?php
+
+namespace App\Tests\Admin;
+
+use App\DataFixtures\tests\ProductFixtures;
+use App\DataFixtures\tests\UserFixtures;
+use App\Entity\Product;
+use App\Entity\User;
+use App\Repository\ProductRepository;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Hautelook\AliceBundle\PhpUnit\RefreshDatabaseTrait;
+use JetBrains\PhpStorm\NoReturn;
+use Liip\TestFixturesBundle\Services\DatabaseToolCollection;
+use Liip\TestFixturesBundle\Services\DatabaseTools\AbstractDatabaseTool;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Faker;
+
+class ProductControllerTest extends WebTestCase
+{
+    use RefreshDatabaseTrait;
+
+    /**
+     * @var AbstractDatabaseTool
+     */
+    protected AbstractDatabaseTool $databaseTool;
+    private KernelBrowser $client;
+    private UserRepository $userRepository;
+    private ProductRepository $productRepository;
+    private string $path = '/admin/product/';
+
+    private EntityManagerInterface $entityManager;
+    private $faker;
+
+    protected function setUp(): void
+    {
+        $this->client = static::createClient();
+        $this->userRepository = static::getContainer()->get('doctrine')->getRepository(User::class);
+        $this->productRepository = static::getContainer()->get('doctrine')->getRepository(Product::class);
+        $this->entityManager = static::getContainer()->get('doctrine')->getManager();
+        $this->databaseTool = static::getContainer()->get(DatabaseToolCollection::class)->get();
+        $this->faker = Faker\Factory::create();;
+    }
+
+    public function test_admin_product_is_not_accessible_anonymously(): void
+    {
+        $this->client->request('GET', $this->path);
+
+        $this->assertResponseStatusCodeSame(302);
+        $this->assertResponseRedirects('/login');
+    }
+
+    public function test_admin_product_index_is_accessible_if_logged(): void
+    {
+        $this->databaseTool->loadFixtures([
+            UserFixtures::class,
+            ProductFixtures::class]);
+
+        $user = $this->userRepository->findOneBy(['email' => 'admin@exempl.es']);
+        $product = $this->productRepository->findAll()[0];
+
+        $this->client->loginUser($user);
+        $crawler = $this->client->request('GET', $this->path);
+
+        $this->assertResponseStatusCodeSame(200);
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains("h1", 'Liste des produits');
+        $this->assertStringContainsString($product->getName(), $crawler->text());
+//        $this->assertStringContainsString($product->getDescription(), $crawler->text());
+//        $this->assertStringContainsString($product->getIngredients(), $crawler->text());
+        $this->assertStringContainsString($product->getPriceHome(), $crawler->text());
+        $this->assertStringContainsString("Modifier", $crawler->text());
+        $this->assertStringContainsString("Ajouter", $crawler->text());
+    }
+
+    #[NoReturn] public function test_admin_can_add_a_product(): void
+    {
+        $this->databaseTool->loadFixtures([
+            UserFixtures::class]);
+
+        $user = $this->userRepository->findOneBy(['email' => 'simple@exempl.es']);
+        $this->client->disableReboot();
+        $this->client->loginUser($user);
+
+        $crawler = $this->client->request('GET', sprintf('%snew', $this->path));
+
+//        Page new is accessible
+        $this->assertResponseStatusCodeSame(200);
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains("h1", 'Ajouter un produit');
+
+        $crawler = $this->client->submitForm('Ajouter', [
+            'product[name]' => $this->faker->name(),
+            'product[description]' => $this->faker->sentence(),
+            'product[ingredients]' => $this->faker->words(3, true),
+            'product[priceHome]' => '4,00',
+            'product[priceAway]' => '3,00',
+            'product[veggie]' => 1,
+            'product[halal]' => 1,
+        ]);
+
+        $this->assertResponseRedirects($this->path, 302);
+        $this->assertSame(1, $this->productRepository->count([]));
+    }
+
+    public function test_admin_can_update_a_product(): void
+    {
+        $this->databaseTool->loadFixtures([
+            UserFixtures::class,
+            ProductFixtures::class]);
+
+        $user = $this->userRepository->findOneBy(['email' => 'admin@exempl.es']);
+        $product = $this->productRepository->findAll()[0];
+
+        $this->client->disableReboot();
+        $this->client->loginUser($user);
+
+        $crawler = $this->client->request('GET', sprintf('%s%s/edit', $this->path, $product->getId()));
+
+//        Page new is accessible
+        $this->assertResponseStatusCodeSame(200);
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains("h1", 'Modifier un produit');
+
+        $name = $this->faker->name();
+        $description = $this->faker->sentence();
+        $ingredients = $this->faker->words(3, true);
+        $price_home = '4,00';
+        $price_away = '3,00';
+        $veggie = $this->faker->boolean;
+        $halal = $this->faker->boolean;
+
+//        Submit the 'new product form'
+        $this->client->submitForm('Modifier', [
+            'product[name]' => $name,
+            'product[description]' => $description,
+            'product[ingredients]' => $ingredients,
+            'product[priceHome]' => $price_home,
+            'product[priceAway]' => $price_away,
+            'product[veggie]' => $veggie,
+            'product[halal]' => $halal,
+        ]);
+
+        $this->assertResponseStatusCodeSame(302);
+        $this->assertResponseRedirects($this->path);
+
+        $product = $this->productRepository->findOneBy(['name' => $name]);
+
+        $this->assertSame($name, $product->getName());
+        $this->assertSame($description, $product->getDescription());
+        $this->assertSame($ingredients, $product->getIngredients());
+        $this->assertSame($price_home, $product->getPriceHome());
+        $this->assertSame($price_away, $product->getPriceAway());
+        $this->assertSame($veggie, $product->isVeggie());
+        $this->assertSame($halal, $product->isHalal());
+
+    }
+
+    public function test_admin_can_delete_a_product(): void
+    {
+        $this->databaseTool->loadFixtures([
+            UserFixtures::class,
+            ProductFixtures::class]);
+
+        $user = $this->userRepository->findOneBy(['email' => 'admin@exempl.es']);
+
+
+        $products = $this->productRepository->findAll();
+        $total = count($products);
+
+        $this->client->disableReboot();
+        $this->client->loginUser($user);
+        $crawler = $this->client->request('GET', sprintf('%s%s/edit', $this->path, $products[0]->getId()));
+
+//        Page new is accessible
+        $this->assertResponseStatusCodeSame(200);
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains("h1", 'Modifier un produit');
+
+        $this->client->submitForm('Supprimer');
+        $this->assertResponseRedirects($this->path, 302);
+
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+        unset($this->databaseTool);
+    }
+}
