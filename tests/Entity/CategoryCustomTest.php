4a8a6342c333e2cd8fd79c309192bbe6cfed3826
Novitz Jean-Philippe <novitz@gmail.com>
2024-03-22 23:42:03 +0100

    Refactor: Test for
    - CategoryDefault Entity
    - CategoryCustom Entity

--- tests/Entity/CategoryCustomTest.php
+++ tests/Entity/CategoryCustomTest.php
@@ -4,16 +4,25 @@ namespace App\Tests\Entity;
 
 use App\Entity\CategoryCustom;
 use App\Entity\CategoryDefault;
+use App\Entity\Product;
 use PHPUnit\Framework\TestCase;
 
 class CategoryCustomTest extends TestCase
 {
     public function test_category_custom_entity(): void
     {
+        $product = new Product();
+
+        $product->setName('Lorem Product');
+        $product->setDescription('Lorem Description');
+        $product->setIngredients('ingredient_1, ingredient_2');
+        $product->setVeggie(true);
         $category = new CategoryCustom();
 
         $category->setName('Lorem');
+        $category->addProduct($product);
 
         $this->assertEquals('Lorem', $category->getName());
+        $this->assertEquals('Lorem Product', $category->getProducts()[0]->getName());
     }
 }
--- tests/Entity/CategoryDefaultTest.php
+++ tests/Entity/CategoryDefaultTest.php
@@ -3,16 +3,27 @@
 namespace App\Tests\Entity;
 
 use App\Entity\CategoryDefault;
+use App\Entity\Product;
 use PHPUnit\Framework\TestCase;
 
 class CategoryDefaultTest extends TestCase
 {
     public function test_category_default_entity(): void
     {
+        $product = new Product();
+
+        $product->setName('Lorem Product');
+        $product->setDescription('Lorem Description');
+        $product->setIngredients('ingredient_1, ingredient_2');
+        $product->setVeggie(true);
+
+
         $category = new CategoryDefault();
 
         $category->setName('Lorem');
+        $category->addProduct($product);
 
         $this->assertEquals('Lorem', $category->getName());
+        $this->assertEquals('Lorem Product', $category->getProducts()[0]->getName());
     }
 }
