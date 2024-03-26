aa24279ee5a674132c51cdcc9327645e30658b41
Novitz Jean-Philippe <novitz@gmail.com>
2024-03-26 03:31:22 +0100

    Refactor Category entity tests
    Only Category Entity.  Tests Needed to adapt.

--- tests/Entity/CategoryCustomTest.php
+++ tests/Entity/CategoryCustomTest.php
@@ -2,8 +2,7 @@
 
 namespace App\Tests\Entity;
 
-use App\Entity\CategoryCustom;
-use App\Entity\CategoryDefault;
+use App\Entity\Category;
 use App\Entity\Product;
 use PHPUnit\Framework\TestCase;
 
@@ -11,18 +10,20 @@ class CategoryCustomTest extends TestCase
 {
     public function test_category_custom_entity(): void
     {
-        $product = new Product();
 
+        $category = new Category();
+        $category->setName('Lorem');
+        $category->setType('custom');
+//        $category->addProduct($product);
+
+        $product = new Product();
         $product->setName('Lorem Product');
         $product->setDescription('Lorem Description');
         $product->setIngredients('ingredient_1, ingredient_2');
         $product->setVeggie(true);
-        $category = new CategoryCustom();
-
-        $category->setName('Lorem');
-        $category->addProduct($product);
+        $product->setCategory($category);
 
         $this->assertEquals('Lorem', $category->getName());
-        $this->assertEquals('Lorem Product', $category->getProducts()[0]->getName());
+        $this->assertEquals('Lorem', $product->getCategory()->getName());
     }
 }
--- tests/Entity/CategoryDefaultTest.php
+++ tests/Entity/CategoryDefaultTest.php
@@ -2,7 +2,7 @@
 
 namespace App\Tests\Entity;
 
-use App\Entity\CategoryDefault;
+use App\Entity\Category;
 use App\Entity\Product;
 use PHPUnit\Framework\TestCase;
 
@@ -10,20 +10,19 @@ class CategoryDefaultTest extends TestCase
 {
     public function test_category_default_entity(): void
     {
-        $product = new Product();
 
+        $category = new Category();
+        $category->setName('Lorem Category');
+        $category->setType('default');
+
+        $product = new Product();
         $product->setName('Lorem Product');
         $product->setDescription('Lorem Description');
         $product->setIngredients('ingredient_1, ingredient_2');
         $product->setVeggie(true);
+        $product->setCategory($category);
 
-
-        $category = new CategoryDefault();
-
-        $category->setName('Lorem');
-        $category->addProduct($product);
-
-        $this->assertEquals('Lorem', $category->getName());
-        $this->assertEquals('Lorem Product', $category->getProducts()[0]->getName());
+        $this->assertEquals('Lorem Category', $category->getName());
+        $this->assertEquals('Lorem Category', $product->getCategory()->getName());
     }
 }
