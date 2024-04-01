cb067c8203b773bd2af4e4d1c54231d13a9a1ca3
Novitz Jean-Philippe <novitz@gmail.com>
2024-04-01 21:42:49 +0200

    Refactor: Edit category
    - refactor CategoryType : displays name only if category is custom or if admin is 'super_admin',
    set rowOrder is only possible if category is ActiveCategory.
    - update edit and _form partials to show details only if necessary.
    - add _active_form partial.

--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -19,36 +19,38 @@ use Symfony\Component\Security\Csrf\TokenStorage\TokenStorageInterface;
 class CategoryType extends AbstractType
 {
 
-    public function __construct(private Security $security, private EntityManagerInterface $entityManager)
+    public function __construct(private Security $security)
     {
 
     }
 
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-//        dd($options['row_order_initial_value']);
-        $builder
-            ->add('name', TextType::class)
-            ->add('rowOrder', IntegerType::class, [
-                'data'=> $options['row_order_initial_value'] ,
-                'mapped' => false
-            ]);
+//        $builder
+//            ->add('name', TextType::class);
+
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) use ($options): void {
+            $form = $event->getForm();;
+
+            if (($event->getData()->getType() === 'custom') || (in_array('ROLE_SUPER_ADMIN', $this->security->getUser()->getRoles()))) {
+                $form->add('name', TextType::class);
+            }
+            if ($this->security->getUser()->getRoles())
+
+                if ($options['row_order_initial_value']) {
+                    $form->add('rowOrder', IntegerType::class, [
+                        'mapped' => false,
+                        'data' => $options['row_order_initial_value']
+                    ]);
+                }
+        });
 
         $builder->addEventListener(
             FormEvents::POST_SUBMIT,
             function (FormEvent $event): void {
                 $data = $event->getData();
                 if ($data->getType() === 'custom')
-                    if ($data->getCategoryInStores()->count() === 0) {
-                        $categoryInStore = new CategoryInStore();
-                        $categoryInStore->setStore($this->security->getUser()->getStore());
-                        $categoryInStore->setRowOrder($event->getForm()->get('rowOrder')->getData());
-                        $this->entityManager->persist($categoryInStore);
-                        $data->addCategoryInStore($categoryInStore);
-                    } else {
-                        $categoryInStore = $data->getCategoryInStores()[0];
-                        $categoryInStore->setRowOrder($event->getForm()->get('rowOrder')->getData());
-                    }
+                    $data->setOwner($this->security->getUser()->getStore());
             }
         );
     }
@@ -57,7 +59,7 @@ class CategoryType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => Category::class,
-            'row_order_initial_value' => 1
+            'row_order_initial_value' => null
         ]);
     }
 
