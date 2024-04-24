1337439ac96b46957ce197e0bc92af9965ac2a77
Novitz Jean-Philippe <novitz@gmail.com>
2024-04-24 23:56:57 +0200

    Refactor MenuController
    Change the way it get the store.
    - was getStore from the current user => always the current user store (always the same)
    - NOW get the store as a controller parameter

--- src/Controller/Menu/MenuController.php
+++ src/Controller/Menu/MenuController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Menu;
 
+use App\Entity\Store;
 use App\Repository\ActiveCategoryRepository;
 use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
@@ -14,10 +15,9 @@ class MenuController extends AbstractController
     public function __construct(private ActiveCategoryRepository $activeCategoryRepository)
     {}
     #[Route('/menu', name: 'app_menu')]
-    public function index(): Response
+    public function index(Store $store): Response
     {
-//        $menu = $this->categoryRepository->findWithProducts();
-        $menu = $this->activeCategoryRepository->findMenu($this->getUser()->getStore());
+        $menu = $this->activeCategoryRepository->findMenu($store);
 
         return $this->render('menu/index.html.twig', [
             'menu' => $menu,
