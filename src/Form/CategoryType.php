4b2691e67f9a9b5332bddb96fdc2f68865170202
Novitz Jean-Philippe <novitz@gmail.com>
2024-05-03 03:23:36 +0200

    Refactor CategoryType
    - add else condition to eventlistener -> case if category is not custom nor user 'superadmin'

--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -3,8 +3,6 @@
 namespace App\Form;
 
 use App\Entity\Category;
-use App\Entity\CategoryCustom;
-use App\Entity\CategoryInStore;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\Form\AbstractType;
@@ -26,16 +24,16 @@ class CategoryType extends AbstractType
 
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-//        $builder
-//            ->add('name', TextType::class);
-
         $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) use ($options): void {
             $form = $event->getForm();;
 
-            if (($event->getData()->getType() === 'custom') || (in_array('ROLE_SUPER_ADMIN', $this->security->getUser()->getRoles()))) {
+            if (($event->getData()->getType() === 'custom') || (in_array('ROLE_SUPERADMIN', $this->security->getUser()->getRoles()))) {
                 $form->add('name', TextType::class);
+            } else {
+                $form->add('name', TextType::class, [
+                    'disabled' => true,
+                ]);
             }
-            if ($this->security->getUser()->getRoles())
 
                 if ($options['row_order_initial_value']) {
                     $form->add('rowOrder', IntegerType::class, [
