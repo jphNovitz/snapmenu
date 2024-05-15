032b907e133bc24520eeaf7c3caa9391a8dd7693
Novitz Jean-Philippe <novitz@gmail.com>
2024-05-15 23:24:53 +0200

    Add variable to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,29 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: [
-    "./assets/**/*.js",
-    "./templates/**/*.html.twig",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./assets/**/*.js",
+        "./templates/**/*.html.twig",
+    ],
+    theme: {
+            colors: {
+                'clear': {
+                    light: '#F5FFF5',
+                    DEFAULT: '#F0FFF0',
+                    dark: '#E0F2E0',
+                },
+                'primary': {
+                    light: '#043404',
+                    DEFAULT: '#021A02',
+                    dark: '#010D01',
+                },
+                'secondary': {
+                    light: '#5D765D',
+                    DEFAULT: '#4A5E4A',
+                    dark: '#394A39',
+                },
+            },
+            extend: {},
+        },
+        plugins: [],
 }
