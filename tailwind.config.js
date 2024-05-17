21bad2e4929e823a9ed9c1d9abdebf7e958b6068
Novitz Jean-Philippe <novitz@gmail.com>
2024-05-17 05:27:28 +0200

    Refactor base template  - remove overflow hidden (y)
    Refactor tailwind config
     - change primary dark color

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,9 +13,9 @@ module.exports = {
                     dark: '#E0F2E0',
                 },
                 'primary': {
-                    light: '#043404',
-                    DEFAULT: '#021A02',
-                    dark: '#010D01',
+                    light: '#1b4332',
+                    DEFAULT: '#043404',
+                    dark: '#081c15',
                 },
                 'secondary': {
                     light: '#5D765D',
@@ -23,6 +23,23 @@ module.exports = {
                     dark: '#394A39',
                 },
             },
+            // colors: {
+            //     'clear': {
+            //         light: '#F5FFF5',
+            //         DEFAULT: '#F0FFF0',
+            //         dark: '#E0F2E0',
+            //     },
+            //     'primary': {
+            //         light: '#043404',
+            //         DEFAULT: '#021A02',
+            //         dark: '#010D01',
+            //     },
+            //     'secondary': {
+            //         light: '#5D765D',
+            //         DEFAULT: '#4A5E4A',
+            //         dark: '#394A39',
+            //     },
+            // },
             extend: {},
         },
         plugins: [],
