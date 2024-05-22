cc88b374fb5ca5f5bc7e9cb628db017c6960fea9
Novitz Jean-Philippe <novitz@gmail.com>
2024-05-22 06:07:05 +0200

    Add nav  Stimulus Controller

---
+++ assets/controllers/nav_controller.js
@@ -0,0 +1,28 @@
+import {Controller} from '@hotwired/stimulus';
+
+/*
+ * This is an example Stimulus controller!
+ *
+ * Any element with a data-controller="hello" attribute will cause
+ * this controller to be executed. The name "hello" comes from the filename:
+ * hello_controller.js -> "hello"
+ *
+ * Delete this file or adapt it for your use!
+ */
+export default class extends Controller {
+    static targets = ['links']
+
+    connect() {
+        console.log('The nav conroller is OK !')
+    }
+
+    trigger(e){
+        console.log("oui")
+        e.stopPropagation()
+        e.preventDefault()
+        this.linksTarget.classList.toggle('-ml-96')
+        // this.linksTarget.getElementsByTagName('a').addEventListener('click', (e)=>{
+        //     this.linksTarget.classList.add('-ml-96')
+        // })
+    }
+}
