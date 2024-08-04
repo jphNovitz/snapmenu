7c5ae575511d55fe56816b3ad8a43d285c54fc80
Novitz Jean-Philippe <novitz@gmail.com>
2024-08-04 23:13:21 +0200

    Refactor Form/text component
    update css classes

---
+++ src/Twig/Components/Form/Text.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Twig\Components\Form;
+
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent]
+final class Text
+{
+
+    public mixed $row;
+    public string $label = "";
+    public string $placeholder = "";
+}
