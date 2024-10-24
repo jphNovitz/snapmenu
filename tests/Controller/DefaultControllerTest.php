f935f90e17e1e12473b20ed8bfc7dda8e3599d6e
Novitz Jean-Philippe <novitz@gmail.com>
2024-10-24 04:01:59 +0200

    Refactor (Default Controller Test)
    remove test for old version, refactor wrong tags 'h1' -> 'div' in header.
    Add missing OpeningsHoursFixtures file.

--- tests/Controller/DefaultControllerTest.php
+++ tests/Controller/DefaultControllerTest.php
@@ -2,6 +2,14 @@
 
 namespace App\Test\Controller;
 
+use App\DataFixtures\tests\CategoryFixtures;
+use App\DataFixtures\tests\OpeningsHoursFixtures;
+use App\DataFixtures\tests\ProductFixtures;
+use App\DataFixtures\tests\StoreFixtures;
+use App\DataFixtures\tests\UserFixtures;
+use App\Entity\Store;
+use App\Repository\StoreRepository;
+use Liip\TestFixturesBundle\Services\DatabaseToolCollection;
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
@@ -9,38 +17,50 @@ class DefaultControllerTest extends WebTestCase
 {
     private KernelBrowser $client;
     private string $path = '/';
+    private StoreRepository $storeRepository;
 
     protected function setUp(): void
     {
         $this->client = static::createClient();
+        $this->storeRepository = static::getContainer()->get('doctrine')->getRepository(Store::class);
+        $this->databaseTool = static::getContainer()->get(DatabaseToolCollection::class)->get();
+
     }
 
     public function test_public_index(): void
     {
+        $this->databaseTool->loadFixtures([
+            UserFixtures::class,
+            StoreFixtures::class,
+            OpeningsHoursFixtures::class,
+            CategoryFixtures::class,
+            ProductFixtures::class]);
+
+        $this->client->disableReboot();
+
+        $store = $this->storeRepository->findAll()[0];
+
         $crawler = $this->client->request('GET', sprintf('%s', $this->path));
         self::assertResponseStatusCodeSame(200);
-        self::assertPageTitleContains('| snapmenu.be');
-        self::assertStringContainsString("Un QR code, tout le menu", $crawler->filter('h1')->text());
+        self::assertPageTitleContains($store->getName());
+        self::assertStringContainsString("Voir le menu", $crawler->text());
+        self::assertStringContainsString("Horaire", $crawler->text());
     }
 
    public function test_public_index_cta_primary(): void
    {
        $crawler = $this->client->request('GET', sprintf('%s', $this->path));
        self::assertResponseStatusCodeSame(200);
-       self::assertStringContainsString("Je suis restaurateur", $crawler->filter('a#cta-primary')->text());
-       $this->client->clickLink('Je suis restaurateur');
+       self::assertStringContainsString("Voir le menu", $crawler->filter('a#cta-primary')->text());
+
+       $link = $crawler->filter('a#cta-primary')->link();
+       $crawler = $this->client->click($link);
+//dd($link->text());
        self::assertResponseStatusCodeSame(200);
-       self::assertPageTitleContains('Inscription');
+       self::assertPageTitleContains('Menu');
+//       dd($crawler);
+       self::assertStringContainsString('Menu', $crawler->filter('h1')->text());
    }
 
-   public function test_public_index_cta_secondary(): void
-   {
-       $crawler = $this->client->request('GET', sprintf('%s', $this->path));
-       self::assertResponseStatusCodeSame(200);
-       self::assertStringContainsString("Voir les restaurants", $crawler->filter('a#cta-secondary')->text());
-       $this->client->clickLink('Voir les restaurants');
-       self::assertResponseStatusCodeSame(200);
-       self::assertStringContainsString("Ces restaurateurs Snappent", $crawler->text());
-   }
 
 }
