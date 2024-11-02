ffb10890e7b9bab05432fe793fedd704f7b7283a
Novitz Jean-Philippe <novitz@gmail.com>
2024-11-02 05:14:58 +0100

    Add (Store controller Action)
    Add route/template for contact infos
    Add translation keys
    Add locale for nelmio_alice

--- src/Controller/Store/StoreController.php
+++ src/Controller/Store/StoreController.php
@@ -14,8 +14,15 @@ class StoreController extends AbstractController
     }
 
     #[Route('/heures-d-ouverture', name: 'app_opening_hours')]
-    public function index(): Response
+    public function schedule(): Response
     {
         return $this->render('store/opening_hours.html.twig');
     }
+
+    #[Route('/infos-contact-acces-menu', name: 'app_infos_contact')]
+    public function infos(): Response
+    {
+        return $this->render('store/infos.html.twig');
+    }
+
 }
