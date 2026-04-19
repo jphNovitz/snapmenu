68e214c10497ae60cd68c45099ca964a79a9875f
Novitz Jean-Philippe <novitz@gmail.com>
2026-04-19 02:41:24 +0200

    feat: add StoreCrudController for managing store entities, replace old StoreController.php

---
+++ src/Controller/Admin/StoreCrudController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Store;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Vich\UploaderBundle\Form\Type\VichImageType;
+
+class StoreCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Store::class;
+    }
+
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextField::new('name')
+                ->setLabel('fields.store.name')
+                ->setColumns(12),
+            TextEditorField::new('description')
+                ->setLabel('fields.store.description')
+                ->setColumns(12),
+            ImageField::new('logoName')
+                ->setBasePath('images/store')
+                ->onlyOnIndex(),
+            TextField::new('logoFile')
+                ->setFormType(VichImageType::class)
+                ->onlyOnForms(),
+            TextField::new('streetName')
+                ->setLabel('fields.store.street')
+                ->setColumns(10),
+            TextField::new('houseNumber')
+                ->setLabel('fields.store.nr')
+                ->setColumns(2),
+            TextField::new('postCode')
+                ->setLabel('fields.store.postal_code')
+                ->setColumns(4),
+            TextField::new('city')
+                ->setLabel('fields.store.city')
+                ->setColumns(8),
+            TextField::new('phoneNumber')
+                ->setLabel('fields.store.phone')
+                ->setColumns(6),
+            TextField::new('vatNumber')
+                ->setLabel('fields.store.vat')
+                ->setColumns(6),
+        ];
+    }
+
+}
