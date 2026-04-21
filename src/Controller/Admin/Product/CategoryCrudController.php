969cc850cc564b7d9c9ce1fb925207a6d8fadbcb
Novitz Jean-Philippe <novitz@gmail.com>
2026-04-22 01:14:09 +0200

    feat: add CategoryCrudController for managing Category entities in admin panel

---
+++ src/Controller/Admin/Product/CategoryCrudController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller\Admin\Product;
+
+use App\Entity\Category;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class CategoryCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Category::class;
+    }
+
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextField::new('name')->setLabel('fields.name')->setColumns(12),
+            IntegerField::new('rowOrder')->setLabel('fields.order')->setColumns(2),
+            BooleanField::new('isActive')->setLabel('fields.active'),
+
+        ];
+    }
+
+}
