a43e8656ebf5f9381369ec508d783a4de543ca9d
Novitz Jean-Philippe <novitz@gmail.com>
2026-04-21 02:40:08 +0200

    feat: add DashboardController for admin panel navigation and configuration

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Controller\Admin\ProductController;
+use App\Controller\Admin\StoreCrudController;
+use App\Controller\Admin\UserCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Attribute\AdminDashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+
+#[AdminDashboard(routePath: '/admin', routeName: 'admin')]
+class DashboardController extends AbstractDashboardController
+{
+    public function index(): Response
+    {
+//        return parent::index();
+
+        // Option 1. You can make your dashboard redirect to some common page of your backend
+        //
+         return $this->redirectToRoute('admin_store_index');
+
+        // Option 2. You can make your dashboard redirect to different pages depending on the user
+        //
+        // if ('jane' === $this->getUser()->getUsername()) {
+        //     return $this->redirectToRoute('...');
+        // }
+
+        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
+        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
+        //
+        // return $this->render('some/path/my-dashboard.html.twig');
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Snapmenu');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkTo(UserCrudController::class, 'Users', 'fas fa-users');
+        yield MenuItem::linkTo(StoreCrudController::class, 'Stores', 'fas fa-store');
+        yield MenuItem::linkTo(ProductController::class,'Products', 'fas fa-box')->setAction('index');
+    }
+}
