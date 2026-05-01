40485173363f8ffc2670c2e5c1ddd1c124aa3281
Novitz Jean-Philippe <novitz@gmail.com>
2026-05-01 04:27:32 +0200

    feat(modal overlay) add copy action and confirmation message

--- assets/controllers/qr_controller.js
+++ assets/controllers/qr_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
-  static targets = ['panel', 'copy']
+  static targets = ['panel', 'alertCopy']
 
   toggle(e) {
     e.stopPropagation()
@@ -11,6 +11,25 @@ export default class extends Controller {
   close(e) {
     e.stopPropagation()
     this.panelTarget.classList.add('hidden')
+  }
+
+  copy(e) {
+    navigator.clipboard.writeText(e.params.uri)
+             .then(() => {
+                  console.log('copié')
+                  this.showCopy()
+               })
+             .catch(err => console.error(err))
+  }
+
+  showCopy(){
+    this.alertCopyTarget.style.opacity = "1"
+    this.alertCopyTarget.style.transform = "translateY(-12px)"
+
+    setTimeout(() => {
+      this.alertCopyTarget.style.opacity = "0"
+      this.alertCopyTarget.style.transform = "translateY(0px)"
+    }, 1500);
   }
 }
 
